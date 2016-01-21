8cdeb21dd341f221b92a1b0df87e01c1d06f2c14
Brian Tsu <tsu.brian.v@gmail.com>
2016-01-21 12:02:09 -0800

    added license tf_binding.sh

--- tf_binding.sh
+++ tf_binding.sh
@@ -11,47 +11,9 @@
 
 cd ~/code/biom262-2016/weeks/week01/data
 
-%%bash --out exercise1
-# Hi, this is my code for exercise1:
-cat tf.bed | awk '$4 ~ /NFKB/ {print}' > tf.nfkb.bed
 
-wc -l tf.nfkb.bed
-echo '--- First 10 lines ---'
-head tf.nfkb.bed
-echo '--- Random 10 lines ---'
-awk -v seed=907 'BEGIN{srand(seed);}{ if (rand() < 0.5 ) {print $0}}'  tf.nfkb.bed | head
-echo '--- Last 10 lines ---'
-tail tf.nfkb.bed
 
-print(exercise1)
 
-%%bash --out exercise2
-# Hi, here is my code for exercise2.  Sincerely, Brian
-cat gencode.v19.annotation.chr22.gtf | awk '$3 ~ /transcript/ {print}' > gencode.v19.annotation.chr22.transcript.gtf
-
-wc -l gencode.v19.annotation.chr22.transcript.gtf
-echo '--- First 10 lines ---'
-head gencode.v19.annotation.chr22.transcript.gtf
-echo '--- Random 10 lines ---'
-awk -v seed=907 'BEGIN{srand(seed);}{ if (rand() < 0.5 ) {print $0}}'  gencode.v19.annotation.chr22.transcript.gtf | head
-echo '--- Last 10 lines ---'
-tail gencode.v19.annotation.chr22.transcript.gtf
-
-print(exercise2)
-
-%%bash --out exercise3
-# Hi, this is my code for exercise3. Sincerely, Brian.
-module load biotools
-bedtools flank -i gencode.v19.annotation.chr22.transcript.gtf -g hg19.genome -s -l 2000 -r 0 > gencode.v19.annotation.chr22.transcript.promoter.gtf
-wc -l gencode.v19.annotation.chr22.transcript.promoter.gtf
-echo '--- First 10 lines ---'
-head gencode.v19.annotation.chr22.transcript.promoter.gtf
-echo '--- Random 10 lines ---'
-awk -v seed=907 'BEGIN{srand(seed);}{ if (rand() < 0.5 ) {print $0}}' gencode.v19.annotation.chr22.transcript.promoter.gtf | head
-echo '--- Last 10 lines ---'
-tail gencode.v19.annotation.chr22.transcript.promoter.gtf
-
-print(exercise3)
 
 %%bash --out exercise4
 # Hi, this is my code for exercise4. Brian.
